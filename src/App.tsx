a978f2c92c7d942983ac6eacec446761597d6b65
victorassiso <victor_assis@poli.ufrj.br>
2021-08-24 21:16:51 -0300

    Adding unsaved changes to last commit
    I forgot to save App.tsx file before commiting

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Header } from './components/Header/header';
-import { NewLeadModal } from './components/NewLeadModal/newLeadModal';
 import { Routes } from './routes';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
-  const [isNewLeadModalOpen, setNewLeadModalOpen] = useState(false);
 
   return (
     <div className="App">
       <BrowserRouter>
         <GlobalStyle />
         <Header />
-        <NewLeadModal
-          isOpen={isNewLeadModalOpen}
-          onRequestClose={()=>setNewLeadModalOpen(false)}
-        />
         <Routes />
       </BrowserRouter>
     </div>
